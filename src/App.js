c8fdf1c96e47189e67cf53bd561eab8f55354d48
Bunty <kr.suraj1602@gmail.com>
2023-09-24 15:46:43 +0530

    final

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Box, Grid } from '@mui/material';
+import React from 'react';
+
+const App = () => {
+	return (
+		<>
+			<Grid container>
+				<Grid item lg={12}>
+					<Box></Box>
+				</Grid>
+			</Grid>
+		</>
+	);
+};
+
+export default App;
